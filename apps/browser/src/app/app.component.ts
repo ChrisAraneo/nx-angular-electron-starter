91fda8d90f7eaecefd0fb1c9a1003f848d77ce4a
Krzysztof Pająk <chris.araneo@gmail.com>
2026-01-25 19:12:48 +0100

    style: format code

--- apps/browser/src/app/app.component.ts
+++ apps/browser/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterModule } from '@angular/router';
   imports: [RouterModule],
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'browser';
